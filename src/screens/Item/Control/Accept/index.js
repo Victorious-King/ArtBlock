91875e958f5647c17044a41897442ee9093df785
Victorious-King <yurhovakimyan1@gmail.com>
2021-10-22 17:45:37 +0300

    final

--- src/screens/Item/Control/Accept/index.js
+++ src/screens/Item/Control/Accept/index.js
@@ -16,25 +16,8 @@ const items = [
 const Accept = ({ className }) => {
   return (
     <div className={cn(className, styles.accept)}>
-      <div className={styles.line}>
-        <div className={styles.icon}></div>
-        <div className={styles.text}>
-          You are about to accept a bid for <strong>C O I N Z</strong> from{" "}
-          <strong>UI8</strong>
-        </div>
-      </div>
-      <div className={styles.stage}>1.46 ETH for 1 edition</div>
-      <div className={styles.table}>
-        {items.map((x, index) => (
-          <div className={styles.row} key={index}>
-            <div className={styles.col}>{x.title}</div>
-            <div className={styles.col}>{x.value}</div>
-          </div>
-        ))}
-      </div>
       <div className={styles.btns}>
-        <button className={cn("button", styles.button)}>Accept bid</button>
-        <button className={cn("button-stroke", styles.button)}>Cancel</button>
+        <button className={cn("button", styles.button)}>Mint</button>
       </div>
     </div>
   );
--- src/screens/Item/index.js
+++ src/screens/Item/index.js
@@ -4,33 +4,7 @@ import styles from "./Item.module.sass";
 import Users from "./Users";
 import Control from "./Control";
 import Options from "./Options";
-
-const navLinks = ["Info", "Owners", "History", "Bids"];
-
-const categories = [
-  {
-    category: "black",
-    content: "art",
-  },
-  {
-    category: "purple",
-    content: "unlockable",
-  },
-];
-
-const users = [
-  {
-    name: "Raquel Will",
-    position: "Owner",
-    avatar: "/images/content/avatar-2.jpg",
-    reward: "/images/content/reward-1.svg",
-  },
-  {
-    name: "Selina Mayert",
-    position: "Creator",
-    avatar: "/images/content/avatar-1.jpg",
-  },
-];
+import Accept from "./Control/Accept";
 
 const Item = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -41,66 +15,26 @@ const Item = () => {
         <div className={cn("container", styles.container)}>
           <div className={styles.bg}>
             <div className={styles.preview}>
-              <div className={styles.categories}>
-                {categories.map((x, index) => (
-                  <div
-                    className={cn(
-                      { "status-black": x.category === "black" },
-                      { "status-purple": x.category === "purple" },
-                      styles.category
-                    )}
-                    key={index}
-                  >
-                    {x.content}
-                  </div>
-                ))}
-              </div>
               <img
                 srcSet="/images/content/item-pic@2x.jpg 2x"
                 src="/images/content/item-pic.jpg"
                 alt="Item"
               />
             </div>
-            <Options className={styles.options} />
           </div>
           <div className={styles.details}>
             <h1 className={cn("h3", styles.title)}>The amazing art</h1>
-            <div className={styles.cost}>
-              <div className={cn("status-stroke-green", styles.price)}>
-                2.5 ETH
-              </div>
-              <div className={cn("status-stroke-black", styles.price)}>
-                $4,429.87
-              </div>
-              <div className={styles.counter}>10 in stock</div>
-            </div>
+            <div className={styles.counter}>Veli T</div>
             <div className={styles.info}>
               This NFT Card will give you Access to Special Airdrops. To learn
               more about UI8 please visit{" "}
-              <a
-                href="https://ui8.net"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://ui8.net
-              </a>
             </div>
-            <div className={styles.nav}>
-              {navLinks.map((x, index) => (
-                <button
-                  className={cn(
-                    { [styles.active]: index === activeIndex },
-                    styles.link
-                  )}
-                  onClick={() => setActiveIndex(index)}
-                  key={index}
-                >
-                  {x}
-                </button>
-              ))}
+            <Accept />
+            <div className={styles.info}>
+              <div className={styles.info}>
+              “mono no aware” is an exploration of change at the borders between existence and emptiness. Shapes and colors appear, twisting and fading gradually into new forms. The evolution of the system creates unpredictable and surprising emergent patterns, as deviations from neat symmetry quickly evolve into chaos. The animation is a cellular automaton running with no pre-defined end — but some patterns don’t have what it takes to survive indefinitely. That’s okay; not everything lasts forever (but the pieces will restart in this case). [Click] to restart. The Level of Detail (LOD) on each animation can be adjusted using [Left Arrow] for less, or [Right Arrow] for more, to allow a customized visual experience and achieve the desired framerate for various devices.
+              </div>
             </div>
-            <Users className={styles.users} items={users} />
-            <Control className={styles.control} />
           </div>
         </div>
       </div>
